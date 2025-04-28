2f8fc6086b6a872910b63a5436b9894853ae7899
Sara <smohaqeqn@gmail.com>
2025-04-28 12:09:29 +0330

    **feat** add and setup @ngneat/svg-generator

---
+++ src/app/shared/modules/custom-icons/icons/Custom-Notification/index.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+/* eslint-disable */
+import { nArrowIcon } from './n-arrow';
+import { nChartIcon } from './n-chart';
+import { nCriticalErrorIcon } from './n-critical-error';
+import { nErrorIcon } from './n-error';
+import { nInfoIcon } from './n-info';
+import { nNotificationIcon } from './n-notification';
+import { nSuccessRequestIcon } from './n-success-request';
+import { nWarningIcon } from './n-warning';
+export const customNotificationIcons = [nArrowIcon, nChartIcon, nCriticalErrorIcon, nErrorIcon, nInfoIcon, nNotificationIcon, nSuccessRequestIcon, nWarningIcon];
