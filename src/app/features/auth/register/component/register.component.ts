fd170913e162099545353fb04bc5ae1120e15cd7
Sara <smohaqeqn@gmail.com>
2025-05-04 17:32:28 +0330

    **feat** custom changes - route navigation  - refresh token

--- src/app/features/auth/register/component/register.component.ts
+++ src/app/features/auth/register/component/register.component.ts
@@ -5,6 +5,7 @@ import {RegisterFormService} from '@features/auth/register/register-form.service
 import {RegisterFormModel} from '@features/auth/register/register-form.model';
 import {RegisterFormAdapter} from '@features/auth/register/register-form.adapter';
 import {RegisterUseCase} from '@application/register.use.case';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,8 +16,10 @@ import {RegisterUseCase} from '@application/register.use.case';
 })
 export class RegisterComponent implements OnInit {
   form!: FormGroup<FormControls<RegisterFormModel>>
+
   constructor(private registerFormService: RegisterFormService,
-              private registerUseCase: RegisterUseCase) {
+              private registerUseCase: RegisterUseCase,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -29,7 +32,11 @@ export class RegisterComponent implements OnInit {
     const raw = this.registerFormService.getRawValue(this.form);
     const data = RegisterFormAdapter.toDomain(raw);
 
-    this.registerUseCase.register(data).subscribe();
+    this.registerUseCase.register(data).subscribe({
+      next: () => {
+        this.router.navigate(['/login']).then();
+      }
+    });
   }
 
 
