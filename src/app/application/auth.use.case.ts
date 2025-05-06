9209dfa3bbdb58dce845193029a94acd3c632490
Sara <smohaqeqn@gmail.com>
2025-05-06 17:37:27 +0330

    **feat** add authFacade and change in authUseCase for clean Architecture

--- src/app/application/auth.use.case.ts
+++ src/app/application/auth.use.case.ts
@@ -1,36 +1,17 @@
 import {IAuthApi} from '@domain/auth/abstracts/auth-api.abstract';
 import {LoginRequest} from '@domain/auth/models/login-request.model';
-import {map, Observable, tap} from 'rxjs';
+import {Observable} from 'rxjs';
 import {LoginResponse} from '@app/domain';
-import {TokenUseCase} from '@application/token.use.case';
 
 export class AuthUseCase {
 
-  constructor(private authApi: IAuthApi,
-              private tokenUseCase:TokenUseCase) {}
+  constructor(private authApi: IAuthApi) {}
 
-  login(data: LoginRequest): Observable<LoginResponse> {
-    return this.authApi.login(data).pipe(
-      tap(res => {
-        this.tokenUseCase.setAccessToken(res.accessToken);
-      })
-    );
+  login(payload: LoginRequest): Observable<LoginResponse> {
+    return this.authApi.login(payload);
   }
 
-  logout(): void {
-    this.tokenUseCase.clearAccessToken();
-  }
-
-  isAuthenticated(): boolean {
-    return !!this.tokenUseCase.getAccessToken();
-  }
-
-  refreshToken(): Observable<void> {
-    return this.authApi.refreshToken().pipe(
-      tap(res => {
-        this.tokenUseCase.setAccessToken(res.accessToken);
-      }),
-      map(() => void 0)
-    );
+  refreshToken(): Observable<LoginResponse> {
+    return this.authApi.refreshToken();
   }
 }
---
+++ src/app/features/auth/auth-facade.service.ts
@@ -0,0 +1,55 @@
+import {Injectable} from '@angular/core';
+import {LoginRequest} from '@app/domain';
+import {BehaviorSubject, map, Observable, tap} from 'rxjs';
+import {AuthUseCase} from '@application/auth.use.case';
+import {TokenUseCase} from '@application/token.use.case';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthFacadeService {
+  private accessToken$ = new BehaviorSubject<string | null>(null);
+
+  constructor(private authUseCase: AuthUseCase,
+              private tokenUseCase: TokenUseCase) {
+  }
+
+  isAuthenticated$: Observable<boolean> = this.accessToken$.pipe(map(token => !!token));
+
+  initFromStorage() {
+    const token = this.tokenUseCase.getAccessToken();
+    this.accessToken$.next(token);
+  }
+
+  login(data: LoginRequest): Observable<void> {
+    return this.authUseCase.login(data).pipe(
+      tap(res => {
+        this.setAccessToken(res.accessToken);
+      }),
+      map(() => void 0)
+    );
+  }
+
+  refreshToken(): Observable<void> {
+    return this.authUseCase.refreshToken().pipe(
+      tap(res => {
+        this.setAccessToken(res.accessToken);
+      }),
+      map(() => void 0)
+    );
+  }
+
+  logout() {
+    this.clearAccessToken();
+  }
+
+  private setAccessToken(token: string) {
+    this.tokenUseCase.setAccessToken(token);
+    this.accessToken$.next(token);
+  }
+
+  private clearAccessToken() {
+    this.tokenUseCase.clearAccessToken();
+    this.accessToken$.next(null);
+  }
+}
