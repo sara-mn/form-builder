94c881c6d2cbcc8318d81d819780a153d1ec0c63
Sara <smohaqeqn@gmail.com>
2025-05-07 13:33:10 +0330

    **refactor** added presentation layer und moved core, feature, shared, layout on it

---
+++ src/app/presentation/features/form-designer/form-designer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormDesignerRoutingModule } from './form-designer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormDesignerRoutingModule
+  ]
+})
+export class FormDesignerModule { }
---
+++ src/app/presentation/features/form-renderer/form-renderer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormRendererRoutingModule } from './form-renderer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormRendererRoutingModule
+  ]
+})
+export class FormRendererModule { }
