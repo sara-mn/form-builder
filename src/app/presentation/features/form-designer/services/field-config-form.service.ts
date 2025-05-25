a03c7a0f38ee55ad4b04910045c2bfbdab8d4d81
Sara <smohaqeqn@gmail.com>
2025-05-25 20:17:11 +0330

    **ref** edited form-designer

--- src/app/presentation/features/form-designer/services/field-config-form.service.ts
+++ src/app/presentation/features/form-designer/services/field-config-form.service.ts
@@ -17,12 +17,20 @@ export class FieldConfigFormService extends BaseFormService<FieldConfigModel> {
 
   createForm(): FormGroup<FormControls<FieldConfigModel>> {
     return this.fb.group({
-      name: this.fb.control('', {validators: [Validators.required]}),
-      label: this.fb.control('',{validators: [Validators.required]}),
-      type: this.fb.control('',{validators: [Validators.required]}),
-      required : this.fb.control(false),
-      options : this.fb.control(null),
-      validators : this.fb.control([]),
+      name: this.fb.control('', { validators: [Validators.required] }),
+      label: this.fb.control('', { validators: [Validators.required] }),
+      type: this.fb.control('', { validators: [Validators.required] }),
+      required: this.fb.control(false),
+      options: this.fb.control(null),
+      validation: this.fb.group({
+        required: this.fb.control(null),
+        minLength: this.fb.control(null),
+        maxLength: this.fb.control(null),
+        min: this.fb.control(null),
+        max: this.fb.control(null),
+        email: this.fb.control(null),
+        pattern: this.fb.control(null)
+      })
     });
   }
 }
--- src/app/presentation/features/form-designer/services/form-designer-facade.service.ts
+++ src/app/presentation/features/form-designer/services/form-designer-facade.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormSchemaModel } from '@app/domain';
+import { FieldTypeEnum, FormSchemaModel, UserRoleEnum } from '@app/domain';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,7 +12,57 @@ export class FormDesignerFacadeService {
 
   getFormById(formId: string): Observable<FormSchemaModel> {
     return new Observable((subscriber) => {
-      subscriber.next({ title: 'form Id 1', fields: [] });
+      subscriber.next({ title: 'form Id 1', fields: [ {
+          name: 'firstName',
+          label: 'نام',
+          type: FieldTypeEnum.Text,
+          required: true,
+          validation: {
+            'minLength': 3,
+            'maxLength': 15,
+            'required': true
+          },
+          access: [UserRoleEnum.Guest, UserRoleEnum.User]
+        }, {
+          name: 'lastName',
+          label: 'نام خانوادگی',
+          type: FieldTypeEnum.Text,
+          required: true,
+          validation: {
+            'minLength': 3,
+            'maxLength': 15,
+            'required': true
+          },
+          access: [UserRoleEnum.Guest, UserRoleEnum.User]
+        }, {
+          name: 'nationalCode',
+          label: 'کد ملی',
+          type: FieldTypeEnum.Number,
+          required: true,
+          validation: {
+            'required': true
+          },
+          access: [UserRoleEnum.Guest, UserRoleEnum.User]
+        },
+          {
+            name: 'gender',
+            label: 'جنسیت',
+            type: FieldTypeEnum.Select,
+            required: true,
+            validation: {
+              'required': false
+            },
+            access: [UserRoleEnum.Guest, UserRoleEnum.User],
+            options: [
+              {
+                key: 'male',
+                value: 'مرد'
+              }, {
+                key: 'female',
+                value: 'زن'
+              }
+            ]
+          }] });
     });
   }
 
