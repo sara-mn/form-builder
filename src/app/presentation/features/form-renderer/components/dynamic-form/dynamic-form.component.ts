3f168550b9d6f03edaa652fdcdd5418300d24f62
Sara <smohaqeqn@gmail.com>
2026-07-05 12:27:58 +0330

    refactor:decouple component from domain data

--- src/app/presentation/features/form-renderer/components/dynamic-form/dynamic-form.component.ts
+++ src/app/presentation/features/form-renderer/components/dynamic-form/dynamic-form.component.ts
@@ -4,82 +4,87 @@ import { FormCreatorService } from '@features/form-renderer/services/form-creato
 import { FormGroup } from '@angular/forms';
 
 @Component({
-    selector: 'app-dynamic-form',
-    templateUrl: './dynamic-form.component.html',
-    styleUrl: './dynamic-form.component.scss',
-    changeDetection: ChangeDetectionStrategy.Eager,
-    standalone: false
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrl: './dynamic-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false
 })
 export class DynamicFormComponent implements OnInit {
-    formJson: FormSchemaModel = {
-        fields: [
-            {
-                name: 'firstName',
-                label: 'نام',
-                type: FieldTypeEnum.Text,
-                required: true,
-                validation: {
-                    minLength: 3,
-                    maxLength: 15,
-                    required: true
-                },
-                access: [UserRoleEnum.Guest, UserRoleEnum.User]
-            },
-            {
-                name: 'lastName',
-                label: 'نام خانوادگی',
-                type: FieldTypeEnum.Text,
-                required: true,
-                validation: {
-                    minLength: 3,
-                    maxLength: 15,
-                    required: true
-                },
-                access: [UserRoleEnum.Guest, UserRoleEnum.User]
-            },
-            {
-                name: 'nationalCode',
-                label: 'کد ملی',
-                type: FieldTypeEnum.Number,
-                required: true,
-                validation: {
-                    required: true
-                },
-                access: [UserRoleEnum.Guest, UserRoleEnum.User]
-            },
-            {
-                name: 'gender',
-                label: 'جنسیت',
-                type: FieldTypeEnum.Select,
-                required: true,
-                validation: {
-                    required: false
-                },
-                access: [UserRoleEnum.Guest, UserRoleEnum.User],
-                options: [
-                    {
-                        key: 'male',
-                        value: 'مرد'
-                    },
-                    {
-                        key: 'female',
-                        value: 'زن'
-                    }
-                ]
-            }
-        ],
-        id: '1',
-        title: 'initial form'
-    };
-    form!: FormGroup;
 
-    constructor(private formCreatorService: FormCreatorService) {}
+  guest: UserRoleEnum = UserRoleEnum.Guest;
+  user: UserRoleEnum = UserRoleEnum.User;
+  admin: UserRoleEnum = UserRoleEnum.Admin;
+  formJson: FormSchemaModel = {
+    fields: [
+      {
+        name: 'firstName',
+        label: 'نام',
+        type: FieldTypeEnum.Text,
+        required: true,
+        validation: {
+          minLength: 3,
+          maxLength: 15,
+          required: true
+        },
+        access: [this.guest, this.user]
+      },
+      {
+        name: 'lastName',
+        label: 'نام خانوادگی',
+        type: FieldTypeEnum.Text,
+        required: true,
+        validation: {
+          minLength: 3,
+          maxLength: 15,
+          required: true
+        },
+        access: [this.guest, this.user]
+      },
+      {
+        name: 'nationalCode',
+        label: 'کد ملی',
+        type: FieldTypeEnum.Number,
+        required: true,
+        validation: {
+          required: true
+        },
+        access: [this.guest, this.user]
+      },
+      {
+        name: 'gender',
+        label: 'جنسیت',
+        type: FieldTypeEnum.Select,
+        required: true,
+        validation: {
+          required: false
+        },
+        access: [this.guest, this.user],
+        options: [
+          {
+            key: 'male',
+            value: 'مرد'
+          },
+          {
+            key: 'female',
+            value: 'زن'
+          }
+        ]
+      }
+    ],
+    id: '1',
+    title: 'initial form'
+  };
+  form!: FormGroup;
 
-    ngOnInit() {
-        this.form = this.formCreatorService.createForm(this.formJson.fields);
-    }
+  constructor(private formCreatorService: FormCreatorService) {
+  }
 
-    onSubmit() {
-        if (this.form.invalid) return;
-    }
+  ngOnInit() {
+    this.form = this.formCreatorService.createForm(this.formJson.fields);
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+  }
 }
